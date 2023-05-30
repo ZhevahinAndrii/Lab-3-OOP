8c0a2d766f92cf36320069bbaedde955b305a4b3
shtrih2k4 <andriizhevahin@gmail.com>
2023-05-30 21:49:01 +0300

    Додав поля до класу клієнта,а саме Id,що ідентифікує користувача на сервері,а також StreamReader & StreamWriter,завдяки яким і відбуватиметься взаємодія між користувачами на сервері

--- TCP-Server/ClientObject.cs
+++ TCP-Server/ClientObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,18 @@ namespace TCP_Server
 {
     internal class ClientObject
     {
+        protected internal string Id { get; }=Guid.NewGuid().ToString();
+        protected internal StreamWriter Writer { get;}
+        protected internal StreamReader Reader { get; }
+        TcpClient client;
+        ServerObject server;
+        public ClientObject(TcpClient client,ServerObject server)
+        {
+            this.client = client;
+            this.server = server;
+            Stream stream = client.GetStream();
+            Reader = new(stream);
+            Writer = new(stream);
+        }
     }
 }
