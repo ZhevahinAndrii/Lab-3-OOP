7f23e6be144d43c3e2d3a31ea34f1d402e0387df
shtrih2k4 <andriizhevahin@gmail.com>
2023-06-01 11:41:48 +0300

    Почав реалізацію серверу, а саме асинхронного процесу прослуховування вхідних підключень та їх реєстрації.

--- TCP-Server/ServerObject.cs
+++ TCP-Server/ServerObject.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
 namespace TCP_Server
 {
     internal class ServerObject
     {
+        TcpListener tcpListener = new(IPAddress.Any, 8888);
+        //сервер для відслідковування і реєстрування підключень за портом 8888
+        List<ClientObject> clients = new();
+        internal async Task ListenAsync()
+        {
+            try
+            {
+                tcpListener.Start();
+                Console.WriteLine("Server is running.Waiting for connections...");
+                while (true)
+                {
+                    TcpClient client = await tcpListener.AcceptTcpClientAsync();
+                    ClientObject clientObject = new(client, this);
+                    clients.Add(clientObject);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message); 
+            }
+        }
     }
 }
